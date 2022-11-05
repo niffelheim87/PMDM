1ba037f5059c27294eb75837718f4edba6d09c2e
Niffelheim <omarkali@gmail.com>
2022-11-05 21:17:22 +0100

    menus funcionando

--- Animaniacos/app/src/main/java/com/ompava/animaniacos/MainActivity.kt
+++ Animaniacos/app/src/main/java/com/ompava/animaniacos/MainActivity.kt
@@ -2,12 +2,17 @@ package com.ompava.animaniacos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.ompava.animaniacos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController2: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,7 +24,29 @@ class MainActivity : AppCompatActivity() {
         val navController =
             (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?)!!.navController
 
-        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
-        NavigationUI.setupWithNavController(binding.toolbar, navController);
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController)
+        NavigationUI.setupWithNavController(binding.toolbar, navController)
+
+        val navHosFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController2 = navHosFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.options1Fragment,
+            R.id.options2Fragment,
+            R.id.options3Fragment
+        )
+        )
+        NavigationUI.setupWithNavController(binding.toolbar, navController2, appBarConfiguration)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, navController2)
+                || super.onOptionsItemSelected(item)
     }
 }
